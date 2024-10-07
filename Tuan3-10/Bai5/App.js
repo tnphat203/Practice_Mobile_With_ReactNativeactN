397e4b4c657cc6b53ca4b8555de67dfb4028fca5
tnphat203tnphat <tnphat203@gmail.com>
2024-10-07 13:51:16 +0700

    Man 1 - Them hinh

--- Tuan3-10/Bai5/App.js
+++ Tuan3-10/Bai5/App.js
@@ -1,20 +1,61 @@
+
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image} from 'react-native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
+
+const stackNavigation = createNativeStackNavigator();
+function APIScreen1({ navigation }) {
+  return (
+    <View style={styles.container}>
+      <View>
+        <View style = {{
+          width: '100%',
+          alignItems: 'center',
+          paddingTop: 82,
+        }}>
+          <Image source={require('./assets/Note.png')} style={{width: 271, height: 271}} />
+        </View>
+      </View>
+      <View>
+
+      </View>
+    </View>
+  );
+}
+
+function APIScreen2({ navigation }) {
+  return (
+    <View style={styles.container}>
+      <Text>API Screen 2</Text>
+    </View>
+  );
+}
+
+function APIScreen3({ navigation }) {
+  return (
+    <View style={styles.container}>
+      <Text>API Screen 3</Text>
+    </View>
+  );
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-     
-     
-    </View>
+    <NavigationContainer>
+      <stackNavigation.Navigator initialRouteName="APIScreen1">
+        <stackNavigation.Screen name="APIScreen1" component={APIScreen1} />
+        <stackNavigation.Screen name="APIScreen2" component={APIScreen2} />
+        <stackNavigation.Screen name="APIScreen3" component={APIScreen3} />
+      </stackNavigation.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    width: '100%',
+    justifyContent: 'space-between',
   },
 });
